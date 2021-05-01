f13792085d14727e18e053ce3773e9a35244c309
Sid Hayoun Lee <sidhlee@gmail.com>
2021-05-01 16:17:28 -0400

    created a namespace & added getMemeData

--- app.js
+++ app.js
@@ -1,1 +1,47 @@
-console.log('anything');
+import data from './data.js';
+
+// create a namespace
+const app = {};
+
+// console.log(data);
+
+// get data from API (100 memes)
+app.getMemeData = () => {
+  const url = 'https://api.imgflip.com/get_memes';
+  fetch(url)
+    .then((response) => {
+      return response.json();
+    })
+    .then((info) => {
+      console.log(info.data.memes[5].url);
+    });
+};
+
+app.getMemeData();
+
+// transform the data
+
+// filter 9 memes
+
+// display meme images
+
+// create list items
+
+// append list items to ul
+
+// bind event listeners to the elements (meme image elements, captioned image, and buttons)
+
+// display the form to the user
+
+// get form input values from user and send them to API
+
+// get response (captioned meme) from API
+
+// display the captioned meme returned from API
+
+// display download/email buttons
+
+// implement download/email functionalities
+
+// declare init function
+// call the init()
